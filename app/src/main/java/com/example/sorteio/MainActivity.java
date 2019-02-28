a6baf80e3c82e9a2f8d967c54c148163cc541fc0
Philipe Cunha <philipe.cunha@bomcontrole.com.br>
2019-02-28 14:35:11 -0300

    Codificação do projeto sorteio.

--- app/src/main/java/com/example/sorteio/MainActivity.java
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.sorteio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultado = findViewById(R.id.resultadoTextView);
+    }
+
+    public void sortearNumero(View view)
+    {
+        int numero = new Random().nextInt(11);
+        resultado.setText("O número selecionado é: " + numero);
     }
 }
